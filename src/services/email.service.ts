e40a94cb99cf047b9c04985699cf3f0fca2b785c
shalev ben hayun <shalev.ben.hayun@agamimp.net>
2024-06-10 10:05:42 +0300

    fixed backend endpoint

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -2,31 +2,31 @@ import axios from "axios";
 import { Email } from "../models/email.model";
 
 const PREFIX = "email";
-const SERVER_URL = "http://localhost:3000"; // Update with your server URL
+const SERVER_URL = "https://sami-backend-j9lt.onrender.com"; // Update with your server URL
 
 function convertDateFormat(email: Email): Email {
   // Create a new Date object from the string
   const date = new Date(email.date);
 
   // Extract the day, month, and year from the date object
-  const day = String(date.getUTCDate() + 1).padStart(2, '0');
-  const month = String(date.getUTCMonth() + 1).padStart(2, '0'); // Months are zero-indexed
+  const day = String(date.getUTCDate() + 1).padStart(2, "0");
+  const month = String(date.getUTCMonth() + 1).padStart(2, "0"); // Months are zero-indexed
   const year = date.getUTCFullYear();
 
   // Format the date as DD/MM/YYYY
   const formattedDate = `${day}/${month}/${year}`;
-  let emailString: string = '';
-const parsedValue = JSON.parse(email.emailAddress);
-    if (Array.isArray(parsedValue)) {
-      emailString = (parsedValue.join(", "));
-    } else {
-      emailString = (parsedValue);
-    }
+  let emailString: string = "";
+  const parsedValue = JSON.parse(email.emailAddress);
+  if (Array.isArray(parsedValue)) {
+    emailString = parsedValue.join(", ");
+  } else {
+    emailString = parsedValue;
+  }
   // Return a new Email object with the date fixed in the new format
   return {
     ...email,
-    emailAddress :`${emailString}, roshcontrol@gmail.com`,
-    date: formattedDate
+    emailAddress: `${emailString}, roshcontrol@gmail.com`,
+    date: formattedDate,
   };
 }
 
